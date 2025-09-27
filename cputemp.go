2e186c9d1106da58afd8741387abd4e8da41439a
northwindlight <twilight_meng@foxmail.com>
2025-09-27 13:58:55 +0800

    修改了返回值，直接返回摄氏度

--- cputemp.go
+++ cputemp.go
@@ -9,20 +9,13 @@ import (
 
 var log = logrus.New()
 
-type TemperatureReading struct {
-	TemperatureC float64
-	TemperatureF float64
-	InstanceName string
-	Success      bool
-}
-
 type wmiThermalSensor struct {
 	InstanceName       string
 	CurrentTemperature uint32
 }
 
 // GetCPUTemperature returns the CPU temperature from the first ACPI thermal sensor
-func GetCPUTemperature() (*TemperatureReading, error) {
+func GetCPUTemperature() (float64, error) {
 	// Set up logger
 	log.SetFormatter(&logrus.TextFormatter{
 		DisableColors: true,
@@ -36,12 +29,12 @@ func GetCPUTemperature() (*TemperatureReading, error) {
 
 	if err != nil {
 		log.Errorf("WMI query failed: %v", err)
-		return nil, fmt.Errorf("unable to get CPU temperature via WMI ACPI: %v", err)
+		return 0.0, fmt.Errorf("unable to get CPU temperature via WMI ACPI: %v", err)
 	}
 
 	if len(sensors) == 0 {
 		log.Error("No thermal sensors found")
-		return nil, fmt.Errorf("unable to get CPU temperature via WMI ACPI: no sensors found")
+		return 0.0, fmt.Errorf("unable to get CPU temperature via WMI ACPI: no sensors found")
 	}
 
 	// Use the first sensor (usually CPU)
@@ -50,31 +43,8 @@ func GetCPUTemperature() (*TemperatureReading, error) {
 	// Convert from Kelvin*10 to Celsius
 	kelvin := float64(sensor.CurrentTemperature) / 10.0
 	celsius := kelvin - 273.15
-	fahrenheit := celsius*1.8 + 32
 
 	log.Infof("CPU temperature: %.1f°C (sensor: %s)", celsius, sensor.InstanceName)
 
-	return &TemperatureReading{
-		TemperatureC: celsius,
-		TemperatureF: fahrenheit,
-		InstanceName: sensor.InstanceName,
-		Success:      true,
-	}, nil
-}
-
-// CheckAdminPermissions verifies if the application has administrator privileges
-func CheckAdminPermissions() bool {
-	var sensors []wmiThermalSensor
-
-	// Try to query a WMI class that requires admin privileges
-	err := wmi.QueryNamespace("SELECT InstanceName FROM MSAcpi_ThermalZoneTemperature",
-		&sensors, "root\\WMI")
-
-	if err != nil {
-		log.Warnf("Admin check failed: %v - application may need administrator privileges", err)
-		return false
-	}
-
-	log.Info("Admin permissions verified")
-	return true
+	return celsius, nil
 }
